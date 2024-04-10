b57e6c70139306fb9ca92082c33387c48e886efc
Mach Buu Chi <sophie@Machs-MacBook-Pro.local>
2024-04-10 11:07:27 +1000

    description of changes made

--- public/script.js
+++ public/script.js
@@ -1,22 +1,56 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+//traditional way
+// function print (msg) {
+//    console.log (msg)
+// }
 
-const ctx = cnv.getContext (`2d`)
+// print (`what's up`)
 
-const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
+//arrow syntax
+// const print = (msg) => {
+//    console.log(msg)
+// }
 
-   requestAnimationFrame (draw_frame)
-}
+// print (`what's up?`)
 
-draw_frame ()
+//remove curly bracket
+// const print = (msg) => console.log(msg)
 
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
+// print (`what's up?`)
+
+//remove parenthesis because only one argument msg
+const print = msg => console.log(msg)
+
+print (`what's up?`)
+
+const ex_arr = [ 'a', 'b', 'c']
+
+//length is stored as a property of the array, describes how many elements in an array, log it into the console
+print (ex_arr.length)
+
+// ex_arr.forEach (print) //grab the elements in the array, print 3 times to the console
+
+ex_arr.forEach (el => console.log (el)) //'el' is an arbitrary variable, declare here, whatever 'el' is, pass it to the console
+
+
+// const cnv = document.getElementById (`cnv_element`) //assigning to variable cnv cnv_element
+// cnv.width = innerWidth //assigning the width of the viewport to canvas width.
+// cnv.height = innerHeight //we want the height of the viewport to be canvas height.
+
+// const ctx = cnv.getContext (`2d`) //context is the thing we use to draw on the canvas
+
+// const draw_frame = () => {
+//    ctx.fillStyle = `turquoise` //addressing the context
+//    ctx.fillRect (0, 0, innerWidth, innerHeight) //in p5, it's bg, make whole thing this color
+
+//    requestAnimationFrame (draw_frame)
+// }
+
+// draw_frame ()
+
+// window.onresize = () => { //resize the window, resize the canvas
+//    cnv.width = innerWidth
+//    cnv.height = innerHeight   
+// }
